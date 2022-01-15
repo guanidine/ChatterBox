dce333e0748bca5dc270e41ed44ae4fe266e34ae
guanidine <zhang010725@gmail.com>
2022-01-15 16:54:15 +0800

    init commit

---
+++ app/src/main/java/com/wzp/chatterbox/MainActivity.java
@@ -0,0 +1,17 @@
+package com.wzp.chatterbox;
+
+import android.view.Window;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginview);
+    }
+}
